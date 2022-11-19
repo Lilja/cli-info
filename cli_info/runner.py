162cf90db86bdb18fe263a1b465c421a06cad2d6
Erik Lilja <erikvlilja@gmail.com>
2022-11-19 13:00:45 +0100

    init commit

---
+++ cli_info/runner.py
@@ -0,0 +1,26 @@
+import re
+import shutil
+from typing import List
+from cli_info.utils import Tool, run_cmd, success, fail
+
+def _process_output_of_cmd(tool: Tool):
+    output = run_cmd(tool)
+    if tool.grep_string:
+        grep_output = re.search(pattern=tool.grep_string, string=output)
+        if grep_output:
+            version = grep_output.group(0)
+            success(tool.name, version)
+    else:
+        success(tool.name, output)
+
+
+def run(tools: List[Tool]):
+    for tool in tools:
+        if tool.use_which:
+            x = shutil.which(tool.name)
+            if x:
+                _process_output_of_cmd(tool)
+            else:
+                fail(tool.name, "not found")
+        else:
+            _process_output_of_cmd(tool)
---
+++ cli_info/utils.py
@@ -0,0 +1,66 @@
+import os
+from typing import Optional
+from dataclasses import dataclass
+
+ESC = "\033["
+RED = "1;31"
+BLUE = "1;34"
+CYAN = "1;36"
+GREEN = "32"
+RESET = "0;0"
+BOLD = "1"
+UNDERLINE = "4"
+ANSI_EXIT_CHAR = "m"
+RESET = ESC + "0;0" + ANSI_EXIT_CHAR
+CHECKMARK = "✅"
+CROSS = "❌"
+
+
+@dataclass
+class Tool:
+    name: str
+    args: str
+    use_which: bool = True
+    pipe_stderr_to_stdout: bool = False
+    grep_string: Optional[str] = None
+    wrap_with_fish: bool = False
+
+
+
+
+def color(*items):
+    return ESC + ";".join(items) + ANSI_EXIT_CHAR
+
+
+def run_cmd(tool: Tool) -> str:
+    cmd = " ".join(
+        [
+            'fish -c "' if tool.wrap_with_fish else "",
+            tool.name,
+            tool.args,
+            '"' if tool.wrap_with_fish else "",
+            "2>&1" if tool.pipe_stderr_to_stdout else "",
+        ]
+    )
+    return os.popen(cmd).read().strip()
+
+
+def success(msg1, msg2):
+    print(
+        CHECKMARK
+        + " "
+        + color(GREEN, BOLD)
+        + msg1
+        + RESET
+        + "\t"
+        + msg2
+    )
+
+
+def fail(msg1, msg2):
+    print(CROSS + " " + color(RED, BOLD) +  msg1 + RESET + "\t"  + msg2)
+
+
+
+
+
